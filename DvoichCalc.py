4f303a813f520078493d264403412d0790e36432
Krestixx <hahdan14@gmail.com>
2024-10-13 02:41:55 +0500

    Add files via upload

---
+++ DvoichCalc.py
@@ -0,0 +1,6 @@
+
+
+decimal_num = int(input('Введите число: '))
+binary_num = format(decimal_num, 'b', )
+print('Ваш ответ: ', binary_num)
+#Калькулятор переводящий в двоичную систему из десятиричной
